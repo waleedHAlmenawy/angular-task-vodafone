a9a5c7f386a0a5d278094e07be9b9e3288af901b
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-07-25 20:16:45 +0300

    feat: implement post UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,7 @@ import { TruncatePipe } from './pipes/truncate.pipe';
     MessageComponent,
     TruncatePipe,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,10 +1,43 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.scss'
+  styleUrl: './post.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({ opacity: 0 })),
+      transition(':enter', [
+        // :enter is alias to 'void => *'
+        animate(200, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        // :leave is alias to '* => void'
+        animate(200, style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('scaleInOut', [
+      state('void', style({ transform: 'scale(0.9)' })),
+      transition(':enter', [
+        animate('300ms ease-out', style({ transform: 'scale(1)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'scale(0.9)' })),
+      ]),
+    ]),
+  ],
 })
 export class PostComponent {
+  showComments = false;
 
+  toggleComments() {
+    this.showComments = !this.showComments;
+  }
 }
