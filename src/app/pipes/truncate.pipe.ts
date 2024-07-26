33ae41c8d6f19cdfa2a0a9225464fc7130f271eb
waleedHAlmenawy <waleed.almenawy@outlook.com>
2024-07-26 11:33:29 +0300

    feat: implement truncate pipe to the posts body

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -1,12 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'truncate'
+  name: 'truncate',
 })
 export class TruncatePipe implements PipeTransform {
-
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): unknown {
+    return value.substring(0, 100) + ' ...';
   }
-
 }
